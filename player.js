431aa7e1efbb324809e9c06888c83636c3255d0d
Hack Reactor Students <forrestbthomas@gmail.com>
2014-06-13 21:49:20 -0700

    implemented draw the enemies, moved to a random location, make a different colored dot for player

--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 var Player = function(gameOptions){
-  this.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z'
+  this.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
   this.fill = '#ff6600';
   this.x = 0;
   this.y = 0;
@@ -13,10 +13,10 @@ Player.prototype.render = function(to){
   this.el = to.append('svg:path')
               .attr('d', this.path)
               .attr('fill', this.fill);
-  this.transform = {
+  this._transform({
     x: this.gameOptions.width * 0.5,
     y: this.gameOptions.height * 0.5
-  };
+  });
   this.setupDragging();
   return this;
 };
@@ -26,11 +26,12 @@ Player.prototype.getX = function(){
 };
 
 Player.prototype.setX = function(x){
+
   var minX = this.gameOptions.padding;
   var maxX = this.gameOptions.width - this.gameOptions.padding;
   x = x <= minX ? minX : x;
   x = x >= maxX ? maxX : x;
-  return this.x;
+  this.x = x;
 };
 
 Player.prototype.getY = function(){
@@ -42,38 +43,40 @@ Player.prototype.setY = function(y){
   var maxY = this.gameOptions.width - this.gameOptions.padding;
   y = y <= minY ? minY : y;
   y = y >= maxY ? maxY : y;
-  return this.y;
+  this.y = y;
 };
 
-Player.prototype.transform = function(opts){
+Player.prototype._transform = function(opts){
+  // TODO: remove the 15 when we are done debugging
   this.angle = opts.angle || this.angle;
   var newX = opts.x || this.x;
   var newY = opts.y || this.y;
   this.setX(newX);
   this.setY(newY);
   var inputVal = 'rotate(' + this.angle + ',' + this.getX() +
-    ',' + this.getY() + ',translate(' + this.getX() +
+    ',' + this.getY() + ') translate(' + this.getX() +
     ',' + this.getY() + ')';
   this.el.attr('transform', inputVal);
 };
 
 Player.prototype.moveAbsolute = function(x,y){
-  this.transform({x:x,y:y});
+  this._transform({x:x,y:y});
 };
 
 Player.prototype.moveRelative = function(dx,dy){
   var newX = this.getX() + dx;
   var newY = this.getY() + dy;
   var newAngle = 360 * (Math.atan2(dy,dx)/(Math.PI*2));
-  this.transform({x:newX,y:newY,angle:newAngle});
+  this._transform({x:newX,y:newY,angle:newAngle});
 };
 
 Player.prototype.setupDragging = function(){
+  var that = this;
   var dragMove = function(){
-    this.moveRelative(d3.event.dx, d3.event.dy);
+    that.moveRelative(d3.event.dx, d3.event.dy);
   };
 
-  var drag = d3.behavior.drag().on('drag', dragMove());
+  var drag = d3.behavior.drag().on('drag', dragMove);
   this.el.call(drag);
 };
 
--- watchout.js
+++ watchout.js
@@ -15,10 +15,20 @@ var axes = {
   x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
   y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height]),
 };
-
-var gameBoard = d3.selectAll('.container').append('svg:svg')
+// debugger
+var gameBoard = d3.select('.container').append('svg:svg')
   .attr('width', gameOptions.width)
   .attr('height', gameOptions.height);
+  // In order to render the enemy objects both the
+  // svg:rect and the enemy objects need to be appended
+  // to the same root (i.e. svg:svg)
+gameBoard.append ('svg:rect')
+  .attr('width', '100%')
+  .attr('height', '100%')
+  .attr('fill', 'white');
+
+
+
 
 var updateScore = function(){
   d3.select('#current-score').text(gameStats.score.toString());
@@ -29,6 +39,94 @@ var updateBestScore = function(){
   d3.select('#best-score').text(gameStats.bestScore.toString());
 };
 
+var players = [];
+players.push(new Player(gameOptions).render(gameBoard));
+
+var createEnemies = function(){
+  return _.range(0,gameOptions.nEnemies).map(function(i){
+    return {id:i, x:Math.random()*100,y:Math.random()*100};
+  });
+};
+
+var render = function(enemyList){
+
+  var enemies = gameBoard.selectAll('circle.enemy').data(enemyList, function(d){
+    return d.id;
+  });
+
+  enemies.enter()
+    .append('svg:circle')
+      .attr('class', 'enemy')
+      .attr('cx', function(enemy){return axes.x(enemy.x);})
+      .attr('cy', function(enemy){return axes.y(enemy.y);})
+      .attr('r', 0)
+      .attr('fill', 'black');
+
+  enemies.exit()
+    .remove();
+
+  var checkCollision = function(enemy, collidedCallback){
+    _.each(players, function(player){
+      var radiusSum = parseFloat(enemy.attr('r')) + player.r;
+      var xDiff = parseFloat(enemy.attr('cx')) - player.x;
+      var yDiff = parseFloat(enemy.attr('cy')) - player.y;
+
+      var separation = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff,2));
+      // TODO why are there no params???
+      if( separation < radiusSum ){
+
+        collidedCallback();
+      }
+    });
+
+  };
+
+  var onCollision = function(enemy, collidedCallback){
+    updateBestScore();
+    gameStats.score = 0;
+    updateScore();
+  };
+
+  var tweenWithCollisionDetection = function(endData){
+    var enemy = d3.select(this);
+    var startPos = {
+      x: parseFloat(enemy.attr('cx')),
+      y: parseFloat(enemy.attr('cy'))
+    };
+    var endPos = {
+      x: axes.x(endData.x),
+      y: axes.y(endData.y)
+    };
+    return function(t){
+      checkCollision(enemy, onCollision);
+      var enemyNextPos = {
+        x: startPos.x + (endPos.x - startPos.x) * t,
+        y: startPos.y + (endPos.y - startPos.y) * t
+      };
+      enemy.attr('cx', enemyNextPos.x)
+        .attr('cy', enemyNextPos.y);
+    };
+  };
+
+  enemies.transition()
+    .duration(500)
+    .attr('r', 10)
+    .duration(2000)
+    .tween('custom', tweenWithCollisionDetection);
+};
 
 
 
+
+var play = function(){
+  var gameTurn = function(){
+    var enemies = createEnemies();
+    render(enemies);
+  };
+
+  gameTurn();
+
+  setInterval(gameTurn, 2000);
+};
+
+play();
